54de139c92c26ded85a983d0686c462dde6b83a1
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2026-01-02 15:20:54 +0530

    Fix port for Render deployment

--- API/server.js
+++ API/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }))
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is runing on port ${port}`));
 
 mongoose.connect(process.env.MONGO_URI)
