c798f19db4a2866fef82671ab15b08da4f051bd0
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2026-01-02 15:31:07 +0530

    Fix port binding for Render

--- API/server.js
+++ API/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   credentials:true
 }))
 const PORT = process.env.PORT || 5000;
-app.listen(port, () => console.log(`server is runing on port ${port}`));
+app.listen(PORT, () => console.log(`server is runing on port ${port}`));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
